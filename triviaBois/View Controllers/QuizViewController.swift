1c547c42b2658fcba11693507fc9c0d2872a2b46
destiny creates <dronesforaliving@gmail.com>
2018-07-24 11:19:31 -0400

    added in property observers and button tags

--- triviaBois/View Controllers/QuizViewController.swift
+++ triviaBois/View Controllers/QuizViewController.swift
@@ -15,25 +15,40 @@ class QuizViewController: UIViewController {
     @IBOutlet weak var answer2Button: UIButton!
     @IBOutlet weak var answer3Button: UIButton!
     @IBOutlet weak var answer4Button: UIButton!
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    var currentQuestion: TriviaQuestion! {
+        didSet {
+            questionLabel.text = currentQuestion.question
+            answer1Button.setTitle(currentQuestion.answers[0], for: .normal)
+            answer2Button.setTitle(currentQuestion.answers[1], for: .normal)
+            answer3Button.setTitle(currentQuestion.answers[2], for: .normal)
+            answer4Button.setTitle(currentQuestion.answers[3], for: .normal)
+            print("the correct answer is: \(currentQuestion.correctAnswer)")
+            
+        }
+    }
+    var score = 0 {
+        didSet {
+            scoreLabel.text = "\(score)"
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-  let newQuestion = TriviaQuestion(question: "Who is the president of the usa in 2018?", answers: ["trump","lil pump","the rock","obama"], correctAnswerIndex: 0)
-        
-        questionLabel.text = newQuestion.question
-        answer1Button.setTitle(newQuestion.answers[0], for: .normal)
-        answer2Button.setTitle(newQuestion.answers[1], for: .normal)
-        answer3Button.setTitle(newQuestion.answers[2], for: .normal)
-        answer4Button.setTitle(newQuestion.answers[3], for: .normal)
-        print("the correct answer is: \(newQuestion.correctAnswer)")
+        let newQuestion = TriviaQuestion(question: "Who is the president of the usa in 2018?", answers: ["trump","lil pump","the rock","obama"], correctAnswerIndex: 0)
         // Do any additional setup after loading the view, typically from a nib.
+    currentQuestion = newQuestion
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    @IBAction func answerButtonTapped(_ sender: UIButton) {
+        print(sender.tag)
+    }
+    
 
 }
 
